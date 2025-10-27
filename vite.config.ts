26f62b3b57e45110c0a804d3af0b77ebaf14b938
João Henrique Servelatti Bitencourt Lemos <162516935+joaohenriqueser@users.noreply.github.com>
2025-10-27 09:06:18 -0300

    Adjust base path based on NODE_ENV

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: './',
+    base: process.env.NODE_ENV === 'production' ? './' : '/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
