b6528d95f4bf4993ab901d98341c6a0d38fa5775
João Henrique Servelatti Bitencourt Lemos <162516935+joaohenriqueser@users.noreply.github.com>
2025-10-27 08:49:31 -0300

    Set base path to './' in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
                 plugins: ['babel-plugin-react-compiler'],
             },
         }),
+        base: './',
         tailwindcss(),
         wayfinder({
             formVariants: true,
