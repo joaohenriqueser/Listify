59b8fcf5a27e0a51e938962be48e13bc512dfc47
João Henrique Servelatti Bitencourt Lemos <162516935+joaohenriqueser@users.noreply.github.com>
2025-10-27 09:14:31 -0300

    Set Vite base path to './' for all environments

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: process.env.NODE_ENV === 'production' ? './' : '/',
+    base: './',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
