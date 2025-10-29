416d2fc368ef60aab4500ca9a746269090ce49be
João Henrique Servelatti Bitencourt Lemos <162516935+joaohenriqueser@users.noreply.github.com>
2025-10-29 15:05:10 -0300

    Update auth-card-layout.tsx

--- resources/js/layouts/auth/auth-card-layout.tsx
+++ resources/js/layouts/auth/auth-card-layout.tsx
@@ -1,4 +1,3 @@
-import AppLogoIcon from '@/components/app-logo-icon';
 import {
     Card,
     CardContent,
@@ -21,31 +20,29 @@ export default function AuthCardLayout({
 }>) {
     return (
         <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-            <div className="flex w-full max-w-md flex-col gap-6">
-                <Link
-                    href={home()}
-                    className="flex items-center gap-2 self-center font-medium"
-                >
-                    <div className="flex items-center space-x-2">
-                        <img
-                            src="/logo-listify.svg" 
-                            alt="Listify Logo"
-                            className="size-full rounded-md object-cover" 
-                        />
-                    </div>
-                </Link>
+            <div className="w-full max-w-md">
+                <Card className="rounded-xl">
+                    <CardHeader className="px-10 pt-8 pb-0 text-center">
+                        <Link
+                            href={home()}
+                            className="flex justify-center font-medium"
+                        >
+                            <div className="flex items-center space-x-2">
+                                <img
+                                    src="/logo-listify.svg"
+                                    alt="Listify Logo"
+                                    className="size-16 rounded-md object-cover mb-4"
+                                />
+                            </div>
+                        </Link>
 
-                <div className="flex flex-col gap-6">
-                    <Card className="rounded-xl">
-                        <CardHeader className="px-10 pt-8 pb-0 text-center">
-                            <CardTitle className="text-xl">{title}</CardTitle>
-                            <CardDescription>{description}</CardDescription>
-                        </CardHeader>
-                        <CardContent className="px-10 py-8">
-                            {children}
-                        </CardContent>
-                    </Card>
-                </div>
+                        <CardTitle className="text-xl">{title}</CardTitle>
+                        <CardDescription>{description}</CardDescription>
+                    </CardHeader>
+                    <CardContent className="px-10 py-8">
+                        {children}
+                    </CardContent>
+                </Card>
             </div>
         </div>
     );
