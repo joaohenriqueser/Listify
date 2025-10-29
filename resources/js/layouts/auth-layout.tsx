c4bac160c9503502583177ce246af8133f2e7867
João Henrique Servelatti Bitencourt Lemos <162516935+joaohenriqueser@users.noreply.github.com>
2025-10-29 12:37:13 -0300

    Update auth-layout.tsx

--- resources/js/layouts/auth-layout.tsx
+++ resources/js/layouts/auth-layout.tsx
@@ -36,7 +36,13 @@ export default function AuthenticatedLayout({ header, children }: AuthenticatedL
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
                                 <Link href={getRoute('dashboard')}>
-                                    <span className="text-xl font-bold text-indigo-600 dark:text-indigo-400">TO-DO List</span>
+                                    <div className="flex items-center space-x-2">
+                                        <svg className="h-9 w-9 text-indigo-600" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                            <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+                                            <path d="M9 12l2 2l4-4"></path>
+                                        </svg>
+                                        <span className="text-2xl font-bold text-gray-800">Listify</span>
+                                    </div>
                                 </Link>
                             </div>
                         </div>
