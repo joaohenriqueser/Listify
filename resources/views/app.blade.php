54d3246d112ba6bcb44cba49b15a20f70c6be994
joaohenriqueser <joao.servelatti@hotmail.com>
2025-10-29 19:51:31 -0300

    testando a logo no navegador

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,20 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}"> 
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title inertia>Listify</title>
 
-        <title inertia>Listify</title>
-        
-                <link rel="icon" href="/logo-listify.svg" type="image/svg+xml">
+    <link rel="icon" href="{{ Vite::asset('resources/logo-listify.svg') }}" type="image/svg+xml">
 
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
-        @viteReactRefresh
-        @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
-        @inertiaHead
-    </head>
+    @viteReactRefresh
+    @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+    @inertiaHead
+</head>
     <body class="font-sans antialiased bg-gray-100 text-gray-900"> 
         @inertia 
     </body>
